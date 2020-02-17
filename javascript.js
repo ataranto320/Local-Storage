74506d2d42c77a2a0791ce4dc5f7c2c9b36ba01e
Austin T <austin.taranto@gmail.com>
2020-02-17 16:50:18 -0500

    addItem function

--- javascript.js
+++ javascript.js
@@ -1,3 +1,18 @@
 const addItems = document.querySelector('.add-items');
   const itemsList = document.querySelector('.plates');
   const items = [];
+
+  function addItem(e) {
+    //   console.log("hello");
+    e.preventDefault();
+    const text = (this.querySelector("[name=item]")).value;
+    const item = {
+        // text: "Item Name",
+        // text: text,
+        text,
+        done: false
+    };
+    console.log(item);
+  }
+
+  addItems.addEventListener("submit", addItem);
